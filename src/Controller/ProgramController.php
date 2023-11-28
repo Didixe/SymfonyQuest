6ed8e94f501b17057fe953053cc8145ae1d11997
Didixe <Adeline.gilet@gmail.com>
2023-11-28 14:51:37 +0100

    symfony quest 6

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,16 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/program/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
-         ]);
-        // return new Response(
-        //     '<html><body>Wild Series Index</body></html>'
-        // );
+        ]);
+    }
+
+    #[Route('/program/{id}/',methods:['GET'], requirements: ['id'=>'\d+'], name: 'show')]
+    public function show(int $id = 1) : Response 
+    {
+        return $this->render('program/show.html.twig', [
+            'id'=> $id,
+        ]);
     }
 }

e1ccf8be8cf3312d3df8f9f48ee1534cbf050021
Didixe <Adeline.gilet@gmail.com>
2023-11-30 10:29:23 +0100

    Quest11

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -23,8 +23,11 @@ class Program
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $poster = null;
 
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
+//    #[ORM\ManyToOne]
+//    #[ORM\JoinColumn(nullable: false)]
+//    private ?Category $category = null;
+
+    #[ORM\ManyToOne(inversedBy: 'programs')]
     private ?Category $category = null;
 
     public function getId(): ?int

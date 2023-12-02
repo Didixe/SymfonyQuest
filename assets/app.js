f969e57ab0a9de0241d033f475b69b5992d8c314
Didixe <Adeline.gilet@gmail.com>
2023-12-02 01:39:42 +0100

    test formulaire + scss

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,17 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import logoPath from './images/fond.jpg';
-let html = `<img src="${logoPath}" alt="fond">`;
+import fondLogoPath  from './images/fond.jpg';
+// let html = `<img src="${fondLogoPath }" alt="fond">`;
+
+import murLogoPath  from './images/mur.jpg';
+// let html = `<img src="${murLogoPath }" alt="mur">`;
+
+let html;
+html = `<img src="${fondLogoPath}" alt="fond">`;
+console.log(html);
+
+html = `<img src="${murLogoPath}" alt="mur">`;
+console.log(html);
 
 console.log('Hello Webpack Encore !')
--- migrations/Version20231201234249.php
+++ migrations/Version20231201234249.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231201140905 extends AbstractMigration
+final class Version20231201234249 extends AbstractMigration
 {
     public function getDescription(): string
     {

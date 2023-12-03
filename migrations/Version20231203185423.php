1241dfc0bbfdccb1ac4c40fac00ddfaf6d402813
Didixe <Adeline.gilet@gmail.com>
2023-12-03 20:11:28 +0100

    authentication and signing in

--- migrations/Version20231203185423.php
+++ migrations/Version20231203185423.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231203141623 extends AbstractMigration
+final class Version20231203185423 extends AbstractMigration
 {
     public function getDescription(): string
     {

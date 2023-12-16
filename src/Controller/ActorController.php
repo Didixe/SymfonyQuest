920f4197c511aec39ca4502b91d71544a5160ced
Didixe <Adeline.gilet@gmail.com>
2023-12-16 19:49:37 +0100

    css

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -48,6 +48,41 @@ class ActorController extends AbstractController
         ]);
     }
 
+    #[Route('/{id}/edit', name: 'app_actor_edit', methods: ['GET', 'POST'])]
+    public function edit(Request$request, Actor $actor ,EntityManagerInterface $entityManager): Response
+    {
+
+        $form = $this->createForm(ActorType::class, $actor);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le nouvel acteur a été modifié');
+
+            return $this->redirectToRoute('app_actor', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('actor/edit.html.twig', [
+            'actor' => $actor,
+            'form' => $form,
+        ]);
+
+    }
+
+    #[Route('/{id}', name: 'app_actor_delete', methods: ['POST'])]
+    public function delete(Request $request, Actor $actor, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$actor->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($actor);
+            $entityManager->flush();
+        }
+
+        $this->addFlash('danger', 'L\'acteur a été supprimé');
+
+        return $this->redirectToRoute('app_actor', [], Response::HTTP_SEE_OTHER);
+    }
 
     #[Route('/show/{id}', name: 'app_actor_show', methods: ['GET'])]
     public function show(Actor $actor): Response
